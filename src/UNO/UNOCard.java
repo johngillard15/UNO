77cb28ce06bbdf4a1205b0ba383d1394d4565832
johngillard15 <johngillard15@gmail.com>
2021-08-13 14:45:09 -0400

    made UNOCard class for UNO specific cards

---
+++ src/UNO/UNOCard.java
@@ -0,0 +1,13 @@
+package UNO;
+
+import com.company.Card;
+
+public class UNOCard extends Card {
+    protected static final String[] COLORS = {"RED", "GREEN", "BLUE", "YELLOW", "WILD"};
+    protected static final String[] VALUES = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "SKIP", "REVERSE", "DRAW_TWO", "COLOR_CHANGE", "DRAW_FOUR"};
+
+    public UNOCard(String color, int value) {
+        super(color, value);
+    }
+}
