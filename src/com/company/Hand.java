0e02e02a5e1b73c615d63dfe401bf2a1438a74ea
johngillard15 <johngillard15@gmail.com>
2021-08-13 14:46:35 -0400

    added Hand class

---
+++ src/com/company/Hand.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import UNO.UNOCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    public List<UNOCard> cards = new ArrayList<>();
+
+    public Hand(){
+
+    }
+}
