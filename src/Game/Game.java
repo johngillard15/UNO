177a6a44268330deeb729dd20901570d5f695ed7
johngillard15 <johngillard15@gmail.com>
2021-08-14 10:48:49 -0400

    updated game to version from Yahtzee, added doc comment

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -3,12 +3,20 @@ package Game;
 import Utilities.InputValidator;
 import com.company.Player;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+/**
+ * <p>Provides a template for a text based console game.</p>
+ *
+ * @since 13/8/2021
+ * @author John
+ * @version 14/8/2021
+ */
+
 public abstract class Game {
-    private static final Scanner scan = new Scanner(System.in);
+    protected static final Scanner scan = new Scanner(System.in);
     protected List<Player> players = new ArrayList<>();
 
     public Game(){
@@ -31,11 +39,9 @@ public abstract class Game {
     protected static int getPlayerCount(){
         return getPlayerCount(1, -1);
     }
-
-    protected static int getPlayerCount(final int MAX){
-        return getPlayerCount(1, MAX);
+    protected static int getPlayerCount(final int MIN){
+        return getPlayerCount(MIN, -1);
     }
-
     protected static int getPlayerCount(final int MIN, final int MAX){
         int numPlayers;
         boolean validNumber;
@@ -52,11 +58,16 @@ public abstract class Game {
 
             numPlayers = Integer.parseInt(input);
 
-            validNumber = numPlayers >= MIN && numPlayers <= MAX;
+            if(MAX == -1)
+                validNumber = MIN >= 1 && numPlayers >= MIN;
+            else
+                validNumber = MIN >= 1 && numPlayers >= MIN && numPlayers <= MAX;
+
             if(!validNumber){
                 System.out.printf("You cannot have %d player%s.\n", numPlayers, numPlayers != 1 ? "s" : "");
                 if(MAX == -1)
-                    System.out.println("You need to have at least 1 player. Please try again with a valid number.");
+                    System.out.printf("You need to have at least %d player%s.\n",
+                            MIN, MIN == 1 ? "" : "s");
                 else
                     System.out.printf("Please pick a number between %d and %d.\n", MIN, MAX);
             }
