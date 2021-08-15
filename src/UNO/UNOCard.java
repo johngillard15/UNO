8010d9bf7d3e5284f06fe358efb1b9f7c0aa07ba
johngillard15 <johngillard15@gmail.com>
2021-08-15 12:50:56 -0400

    changed constructor value to String to match array

--- src/UNO/UNOCard.java
+++ src/UNO/UNOCard.java
@@ -7,7 +7,7 @@ public class UNOCard extends Card {
     protected static final String[] VALUES = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
             "SKIP", "REVERSE", "DRAW_TWO", "COLOR_CHANGE", "DRAW_FOUR"};
 
-    public UNOCard(String color, int value) {
+    public UNOCard(String color, String value) {
         super(color, value);
     }
 }
