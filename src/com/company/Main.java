f2ac08731902469982187fc9b586a53859cf405c
johngillard15 <johngillard15@gmail.com>
2021-08-15 14:40:40 -0400

    main now plays UNO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import UNO.UNO;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        UNO uno = new UNO();
+        uno.play();
     }
 }
