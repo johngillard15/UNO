1e1e1c5d5321396bc8f46fa3ae9b2199a1906809
johngillard15 <johngillard15@gmail.com>
2021-08-15 14:27:13 -0400

    added methods to handle adding and removing cards from list

--- src/com/company/Hand.java
+++ src/com/company/Hand.java
@@ -6,9 +6,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
-    public List<UNOCard> cards = new ArrayList<>();
+    public List<Card> cards = new ArrayList<>();
 
     public Hand(){
 
+    }
+
+    public void addCard(String suit, String value){
+        cards.add(Card.createCard(suit, value));
+    }
+
+    public void addCard(Card card){
+        cards.add(card);
+    }
+
+    public void removeCard(){
+        cards.remove(0);
+    }
+
+    public void removeCard(int index){
+        cards.remove(index);
+    }
+
+    public void removeCard(Card card){
+        cards.remove(card);
     }
 }
