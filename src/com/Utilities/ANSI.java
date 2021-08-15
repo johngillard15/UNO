1aebfa0391bc1629b35c4073f12037b148149e82
johngillard15 <johngillard15@gmail.com>
2021-08-15 19:04:00 -0400

    created ANSI interface to store escape codes

---
+++ src/com/Utilities/ANSI.java
@@ -0,0 +1,21 @@
+package com.Utilities;
+
+public interface ANSI {
+    String RESET = "\u001B[0m";
+    String BLACK = "\u001B[30m";
+    String RED = "\u001B[31m";
+    String GREEN = "\u001B[32m";
+    String YELLOW = "\u001B[33m";
+    String BLUE = "\u001B[34m";
+    String PURPLE = "\u001B[35m";
+    String CYAN = "\u001B[36m";
+    String WHITE = "\u001B[37m";
+    String BLACK_BG = "\u001B[40m";
+    String RED_BG = "\u001B[41m";
+    String GREEN_BG = "\u001B[42m";
+    String YELLOW_BG = "\u001B[43m";
+    String BLUE_BG = "\u001B[44m";
+    String PURPLE_BG = "\u001B[45m";
+    String CYAN_BG = "\u001B[46m";
+    String WHITE_BG = "\u001B[47m";
+}
