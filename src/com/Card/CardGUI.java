5a15958ceec9dae06ac55f7106f9a7f249f17b23
johngillard15 <johngillard15@gmail.com>
2021-08-15 19:08:39 -0400

    changed ANSI escape codes from local to ANSI interface constants

--- src/com/Card/CardGUI.java
+++ src/com/Card/CardGUI.java
@@ -1,11 +1,11 @@
 package com.Card;
 
+import com.Utilities.ANSI;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardGUI {
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    public static final String ANSI_RESET = "\u001B[0m";
     public static List<String> cards = new ArrayList<>();
 
     public CardGUI(){
@@ -21,11 +21,23 @@ public class CardGUI {
     }
 
     public static void cardTest(){
-        System.out.println(ANSI_BLUE_BACKGROUND + "Hello, World!" + ANSI_RESET);
+        System.out.println(ANSI.YELLOW_BG + ANSI.BLACK + "Hello, World!" + ANSI.RESET);
+
+        String ONE = "1";
+        System.out.printf(
+                ANSI.RED + """
+                        ╭─────────╮
+                        │%s        │
+                        │         │
+                        │    %s    │
+                        │         │
+                        │        %s│
+                        ╰─────────╯
+                        """ + ANSI.RESET, ONE, ONE, ONE);
 
         String TWO = "2";
         System.out.printf(
-                """
+                ANSI.BLUE + """
                         ╭─────────╮
                         │%s        │
                         │         │
@@ -33,13 +45,13 @@ public class CardGUI {
                         │         │
                         │        %s│
                         ╰─────────╯
-                        """, TWO, TWO, TWO);
+                        """ + ANSI.RESET, TWO, TWO, TWO);
 
         String SIX = "6";
         String SIX_LINE = "̲6";
-        String SIX_DOWN = "9";
+        String SIX_TURNED = "9";
         System.out.printf(
-                """
+                ANSI.GREEN + """
                         ╭─────────╮
                         │%s        │
                         │         │
@@ -47,13 +59,13 @@ public class CardGUI {
                         │         │
                         │        %s│
                         ╰─────────╯
-                        """, SIX, SIX_LINE, SIX_DOWN);
+                        """ + ANSI.RESET, SIX, SIX_LINE, SIX_TURNED);
 
         String NINE = "9";
         String NINE_LINE = "̲9"; // has a "combining low line" or "non-spacing underscore"
-        String NINE_DOWN = "6";
+        String NINE_TURNED = "6";
         System.out.printf(
-                """
+                ANSI.YELLOW + """
                         ╭─────────╮
                         │%s        │
                         │         │
@@ -61,7 +73,7 @@ public class CardGUI {
                         │         │
                         │        %s│
                         ╰─────────╯
-                        """, NINE, NINE_LINE, NINE_DOWN);
+                        """ + ANSI.RESET, NINE, NINE_LINE, NINE_TURNED);
 
         String SKIP = " ⃠"; // uses a thin space
         System.out.printf(
@@ -69,7 +81,7 @@ public class CardGUI {
                         ╭─────────╮
                         │%s        │
                         │         │
-                        │    %s     │
+                        │    %s      │
                         │         │
                         │       %s │
                         ╰─────────╯
@@ -81,10 +93,37 @@ public class CardGUI {
                         ╭─────────╮
                         │%s       │
                         │         │
-                        │    %s    │
+                        │    %s     │
                         │         │
                         │       %s│
                         ╰─────────╯
                         """, REVERSE, REVERSE, REVERSE);
+
+        String DRAW_FOUR = ANSI.WHITE + "+4" + ANSI.RESET + ANSI.BLACK; // uses hair space
+        System.out.printf(
+                ANSI.BLACK + """
+                        ╭─────────╮
+                        │%s       │
+                        │         │
+                        │    %s     │
+                        │         │
+                        │       %s│
+                        ╰─────────╯
+                        """ + ANSI.RESET, DRAW_FOUR, DRAW_FOUR, DRAW_FOUR);
+
+        String D4_TOP = ANSI.BLUE + "▮ " + ANSI.RESET + ANSI.BLACK;
+        String D4_LEFT = ANSI.RED + "▮" + ANSI.RESET + ANSI.BLACK;
+        String D4_RIGHT = ANSI.GREEN + "▮  " + ANSI.RESET + ANSI.BLACK;
+        String D4_BOT = ANSI.YELLOW + "▮ " + ANSI.RESET + ANSI.BLACK;
+        System.out.printf(
+                ANSI.BLACK + """
+                        ╭─────────╮
+                        │%s       │
+                        │    %s   │
+                        │   %s%s  │
+                        │    %s   │
+                        │       %s│
+                        ╰─────────╯
+                        """ + ANSI.RESET, DRAW_FOUR, D4_TOP, D4_LEFT, D4_RIGHT, D4_BOT, DRAW_FOUR);
     }
 }
