915256a449b56c24753bbd2e107c062e2084321e
johngillard15 <johngillard15@gmail.com>
2021-08-16 16:53:56 -0400

    made fields public

--- src/com/UNO/UNOCard.java
+++ src/com/UNO/UNOCard.java
@@ -3,11 +3,13 @@ package com.UNO;
 import com.Card.Card;
 
 public class UNOCard extends Card {
-    protected static final String[] COLORS = {"RED", "GREEN", "BLUE", "YELLOW", "WILD"};
-    protected static final String[] VALUES = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    public static final String[] COLORS = {"RED", "GREEN", "BLUE", "YELLOW", "WILD"};
+    public static final String[] VALUES = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
             "SKIP", "REVERSE", "DRAW_TWO", "COLOR_CHANGE", "DRAW_FOUR"};
 
     public UNOCard(String color, String value) {
         super(color, value);
     }
+
+    // maybe make new sort by suit to compare indices?
 }
