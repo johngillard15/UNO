6047c8ceff6dc8060626846d27baa0171cf83dd5
johngillard15 <johngillard15@gmail.com>
2021-08-15 22:24:00 -0400

    removed cardTest

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,10 @@
 package com.company;
 
-import com.Card.CardGUI;
 import com.UNO.UNO;
 
 public class Main {
     public static void main(String[] args) {
         UNO uno = new UNO();
         uno.play();
-
-//        CardGUI.cardTest();
     }
 }
