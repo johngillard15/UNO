71860fbc8dd49e7e9a5603de6073c6255042a457
johngillard15 <johngillard15@gmail.com>
2021-08-15 22:10:00 -0400

    added showCard to print a single card

--- src/com/Card/CardGUI.java
+++ src/com/Card/CardGUI.java
@@ -1,8 +1,5 @@
 package com.Card;
 
-import com.UNO.UNOCard;
-import com.Utilities.ANSI;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -20,6 +17,10 @@ public class CardGUI {
         else
             return UnoCardGUI.getColorCard(card.suit, card.value);
 
+    }
+
+    public static void showCard(Card card){
+        System.out.println(getCard(card));
     }
 
     public static void showHand(List<Card> hand){
