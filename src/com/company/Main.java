520a101a73d39f809571b5895ef8152f5c0e5e3b
johngillard15 <johngillard15@gmail.com>
2021-08-15 21:42:23 -0400

    updated to use uno.play

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,13 @@
 package com.company;
 
 import com.Card.CardGUI;
-import com.Card.UnoCardGUI;
 import com.UNO.UNO;
 
 public class Main {
     public static void main(String[] args) {
-//        UNO uno = new UNO();
-//        uno.play();
+        UNO uno = new UNO();
+        uno.play();
 
 //        CardGUI.cardTest();
-        System.out.println(UnoCardGUI.getWildCard("DRAW_FOUR"));
-        System.out.println(UnoCardGUI.getWildCard("COLOR_CHANGE"));
-        System.out.println(UnoCardGUI.getColorCard("RED", "6"));
-        System.out.println(UnoCardGUI.getColorCard("BLUE", "8"));
     }
 }
